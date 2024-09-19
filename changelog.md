80210156bb6f4ab2eb532fdaead52d4825773aef
Tony Bark <noreply@tonybark.com>
2024-09-19 00:18:24 -0400

    New launch browser component - Now possible to click links that launch the browser from the backend - Version scheme change to account for different methods with desktop applications

---
+++ changelog.md
@@ -0,0 +1,23 @@
+# Change Log
+
+## 0.104.2
+
+- Clickable links that open the default broswer.
+
+### Version sceheme change
+
+Three digits in the version were moved from the patch to the minor section (i.e., `0.Y.100` to `0.100.Z`) due to the difference in ways of desktop applications are developed, which I never needed to account for until now. It was easier to get away with putting it in the patch for CLI tools and libraries because the core user experience never changed, despite frequent internal changes.
+
+## 0.104.0 (0.1.104)
+
+- New buttons for major terminals and rail cams.
+- Version displayed above credits.
+
+## 0.102.0 (0.1.102)
+
+- Redesigned icons
+- Added banner above form
+
+## 0.100.0 (0.1.101)
+
+- First public prototype
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use webbrowser;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn station(name: &str) -> String {
-    format!(
-        "https://dixielandsoftware.net/cgi-bin/solari_relay.pl?data={}",
-        name
-    )
+fn open_browser(address: &str) {
+    webbrowser::open(address).expect("Failed to open defualt browser.");
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![station])
+        .invoke_handler(tauri::generate_handler![open_browser])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
